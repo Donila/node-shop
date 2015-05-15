181a732123a638ef730c66aba12e6bb5ca15a1c4
Daniil Dziaruhin <daniil.dziaruhin@pointsource.com>
2015-05-15 18:05:54 +0300

    angular-fullstack initial code + configured mongolab database connection for development

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://Developer:Password123@ds063789.mongolab.com:63789/node-shop-dev'
+  },
+
+  seedDB: true
+};
